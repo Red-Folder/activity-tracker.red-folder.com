91bc75be2ca5af24dcdd5966f96ee41b3f7d7906
Red Folder <markbryantaylor@yahoo.co.uk>
2018-12-24 14:15:23 +0000

    Added styling to the splash page

--- src/spashPage.js
+++ src/spashPage.js
@@ -6,8 +6,19 @@ import logo from './rfc-folder-logo.png';
 
 const SplashPage = () => (
     <div id='splash-page'>
-        <img src={logo} alt="RFC Logo" />
-        <a class='btn btn-primary' href='/activity'>Login</a>
+        <div>
+            <img id='splash-page-logo' src={logo} alt="RFC Logo" class="img-circle" />
+        </div>
+        <h1>RFC Activity Approval</h1>
+        <p>
+            This is a private system intended for use by Red Folder Consultancy Ltd staff only.
+        </p>
+        <p>
+            Those with authorised credentials should use the login button below.
+        </p>
+        <div>
+            <a class='btn btn-primary' href='/activity'>Login</a>
+        </div>
     </div>
 );
 export default SplashPage;
